b81eb69d5f7867a13d75978dfcdb5bec60d0b17c
suvorika <iloveirokezzz@gmail.ru>
2023-06-26 16:56:32 +0300

    fixe model

--- practice_django/blog/admin.py
+++ practice_django/blog/admin.py
@@ -9,4 +9,8 @@ from blog.models import Post
 class PageAdmin(admin.ModelAdmin):
     list_display = [
         "title",
+        "date_created",
+        "date_update",
+        "author",
     ]
+    prepopulated_fields = {"slug": ("title",)}
--- practice_django/blog/models.py
+++ practice_django/blog/models.py
@@ -1,5 +1,38 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+
+# from datetime import timezone
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    class Meta:
+        verbose_name = "Создать пост"
+        verbose_name_plural = "Создать посты"
+
+    title = models.CharField(
+        max_length=200, help_text="не более 200 символов", db_index=True
+    )
+    content = RichTextField(
+        max_length=5000, blank=True, null=True, help_text="не более 5000 символов"
+    )
+    date_created = models.DateTimeField(default=timezone.now)
+    date_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # в url при использовании slug обязательно добавить id + get_absolute_url()
+    slug = models.SlugField(max_length=50, unique=True)  # unique=True
+    likes = models.ManyToManyField(User, related_name="postcomment", blank=True)
+    reply = models.ForeignKey(
+        "self", null=True, related_name="reply_ok", on_delete=models.CASCADE
+    )
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.title
