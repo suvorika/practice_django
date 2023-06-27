7d2cb3602d7c211db7e70c5d6a34b02003f90ac0
suvorika <iloveirokezzz@gmail.ru>
2023-06-27 16:53:19 +0300

    expanded

--- practice_django/practice_django/settings.py
+++ practice_django/practice_django/settings.py
@@ -217,7 +217,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "my_site/templates")
+    os.path.join(BASE_DIR, "other_static"),
 ]
 
 STATICFILES_FINDERS = [
